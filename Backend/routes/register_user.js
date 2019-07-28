9e340d392265d8f15c15b921f559dfcc2fc84cd8
Nilay Gupta <nilaygupta1998@gmail.com>
2019-07-28 15:38:59 +0530

    registering on id

--- Backend/routes/register_user.js
+++ Backend/routes/register_user.js
@@ -4,17 +4,21 @@ const _ =require('lodash');
 const {Student, validateID, validateUser, validateLogin} = require('../models/student');
 const bcrypt=require('bcrypt');
 
-router.post('/user', async(req,res) =>{
+router.put('/user', async(req,res) =>{
     const { error } = validateUser(req.body); 
     if (error) return res.json({status:'0', message: error.details[0].message});
 
     let student = await Student.findOne({ email: req.body.email });
     if (student) return res.json({status:'0',message:'Student already registered.'});
 
-    student = new Student(_.pick(req.body, ['name', 'email', 'password','rollNumber']));
+    student = await Student.findOne({ id:req.body.id});
+    if(!student || student.email) return res.json({status:'0', message:'Wrong Id entered'});
+
     const salt = await bcrypt.genSalt(10);
-    student.password = await bcrypt.hash(student.password, salt);
-    await student.save();
+    req.body.password = await bcrypt.hash(req.body.password, salt);
+
+    student = await Student.findOneAndUpdate({id:req.body.id},(_.pick(req.body, ['name', 'email', 'password','rollNumber'])));
+    
 
     res.json({status:'1',message:'Registeration Success'});
 });
