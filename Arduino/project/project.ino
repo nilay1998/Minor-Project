29c29ab996ddbc1e6cbf8a676cdf63ba3a74fad0
Nilay Gupta <nilaygupta1998@gmail.com>
2019-08-06 18:57:26 +0530

    id 0 post req fix

--- Arduino/project/project.ino
+++ Arduino/project/project.ino
@@ -130,7 +130,10 @@ void Enroll()
           lcd.setCursor(0,1);
           lcd.print("sful");
           String val=String(enrollid)+":enroll";
-          mySerial.print(val);
+          if(enrollid!=0)
+          {
+            mySerial.print(val);
+          }
           delay(1000);
         }
         else
@@ -190,7 +193,7 @@ void Identify()
     {//if the fingerprint matches, provide the matching template ID
       lcd.clear();
       lcd.print("Verified ID:");
-      lcd.println(id);
+      lcd.println(id+ " ");
       if(id==0)
       return Enroll();
       String val=String(id)+":add";
